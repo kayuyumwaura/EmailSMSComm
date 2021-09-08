a77cb4b219a8720a02bc43310f39eb240c5d1bbb
kayuyu <kayuyu.mwaura@aku.edu>
2021-09-08 16:43:14 +0300

    adjusted smtp settings

--- EmailSMSCommunication/Controllers/HomeController.cs
+++ EmailSMSCommunication/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using EmailSMSCommunication.Models;
+using Intuit.Ipp.Core.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,6 +14,44 @@ namespace EmailSMSCommunication.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Index(EmailModel model)
+        {
+            using (var client = new HttpClient())
+            {
+                //Passing service base url    
+                client.BaseAddress = new Uri(BaseUrl);
+
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format    
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service using HttpClient    
+                HttpResponseMessage Res = await client.PostAsJsonAsync("api/email/send-email", model);
+
+                //Checking the response is successful or not which is sent using HttpClient    
+                if (Res.IsSuccessStatusCode)
+                {
+                    return View("Success");
+                }
+                else
+                {
+                    return View("Error");
+                }
+            }
+        }
+
+        public ActionResult Success()
+        {
+            return View();
+        }
+
+        public ActionResult Error()
         {
             return View();
         }
